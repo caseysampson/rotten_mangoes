7f858829145bdcf628734370aebe86724a843fe4
caseysampson <caseysampson@hotmail.com>
2014-11-25 01:17:15 +0000

    displaying validations errors in movies form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = 'd4b3b0377ee3056ae538d1d2005229be0e495ae2716ace3cd8cf18673ed5f01ece250deefb10f834e2b2f1c7a84be2a37690b24a8645126a45ec1d7b23010663'
