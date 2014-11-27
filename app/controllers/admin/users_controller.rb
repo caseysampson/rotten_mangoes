be09f5af26497f9d3023ba0f1b27e64938e0498e
caseysampson <caseysampson@hotmail.com>
2014-11-27 21:08:25 +0000

    admin can delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -41,6 +41,12 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
   protected
 
   def user_params
