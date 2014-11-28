36c0bbc1a58f9f09f4956115c73be6d1969cc976
caseysampson <caseysampson@hotmail.com>
2014-11-28 23:05:39 +0000

    search complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,31 +15,28 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_future
 
-  scope :search_results, -> (params) {
-    if params[:title].present?
-      find_by_title[:title]
-    elsif params[:director].present?
-      find_by_director[:director]
-    # else
-    #   find_by_duration
+  scope :search_results, -> (params) do
+    if params[:title].present? || params[:director].present?
+      self.where("movies.title LIKE ? OR movies.director LIKE ?", params[:title], params[:director])
+    else
+     find_by_duration(params[:duration])
     end
-  }
+  end
 
       
-  scope :find_by_director, ->(director) { where("director", director) }
-  scope :find_by_title, ->(title) { where("title LIKE ?", title) }
+  # scope :find_by_director, ->(director) { where("director", director) }
+  # scope :find_by_title, ->(title) { where("movies.title LIKE ?", title) }
   scope :find_by_duration, ->(duration) do
     case duration
-      when 1
-        where("runtime_in_minutes < ?", 90)
-      when 2
-        where("runtime_in_minutes BETWEEN ? AND ?", 90, 120)
-      when 3
-        where("runtime_in_minutes > ?", 120)
-      else
-        where("runtime_in_minutes < ?", 0)
-      end
-    end
+    when '1'
+      where("runtime_in_minutes < ?", 90)
+    when '2'
+      where("runtime_in_minutes BETWEEN ? AND ?", 90, 120)
+    when '3'
+      where("runtime_in_minutes > ?", 120)
+    else
+      where("runtime_in_minutes < ?", 0)
+    end  
   end
 
   def review_average
